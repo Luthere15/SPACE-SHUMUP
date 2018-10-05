ccab713abc3c66935750c61af0220b2327400e09
Luthere15 <42876725+Luthere15@users.noreply.github.com>
2018-10-05 16:38:54 -0400

    557

--- Shoot Shumup/Assets/__Scripts/Hero.cs
+++ Shoot Shumup/Assets/__Scripts/Hero.cs
@@ -10,7 +10,7 @@ public class Hero : MonoBehaviour
 
     public float speed = 30;
     public float rollMult = -45;
-    public float pitchMult;
+    public float pitchMult = 30;
 
     [Header("Set Dynamically")]
 
@@ -35,12 +35,12 @@ public class Hero : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        float xAxis = Input.GetAxis("horizontal");
+        float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
 
-        Vector2 pos = transform.position;
-        pos.x +=xAxis + speed *Time.deltaTime;
-        pos.y += yAxis + speed * Time.deltaTime;
+        Vector3 pos = transform.position;
+        pos.x +=xAxis * speed *Time.deltaTime;
+        pos.y +=yAxis * speed *Time.deltaTime;
         transform.position = pos;
 
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
